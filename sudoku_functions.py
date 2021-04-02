51e6bcb47600b2a79d08dbcad00a742c4d83939b
Ben Say <benbsay@gmail.com>
2021-04-02 13:57:21 -0700

    methods to read array, scan probabilities

--- sudoku_functions.py
+++ sudoku_functions.py
@@ -14,9 +14,50 @@ def print_entries_array_to_console(array):
 
 def solve(array): # this is where the sudoku gets solved
     print("starting solve")
+
+    #initialize array which will eventually contain solution with user entries
     solving = array
     print_entries_array_to_console(solving)
-    probability_array # HERE------------------------------------------------------------------
+
+    #create arrays for probability storage
+    probability_array_row = [space_probabilities() for i in range(9)]
+    probability_array_board = [probability_array_row for i in range(9)]
+
+    """
+    probability_array_board
+    index 0   |  9 space probabilities (one probability_array_row)
+    index 1   |  9 space probabilities (one probability_array_row)
+    index 2   |  9 space probabilities (one probability_array_row)
+    index 3   |  9 space probabilities (one probability_array_row)
+    index 4   |  9 space probabilities (one probability_array_row)
+    index 5   |  9 space probabilities (one probability_array_row)
+    index 6   |  9 space probabilities (one probability_array_row)
+    index 7   |  9 space probabilities (one probability_array_row)
+    index 8   |  9 space probabilities (one probability_array_row)
+    """
+
+    #store current numbers, spaces are considered all true here
+    for i in range(9):
+        for j in range(9):
+            probability_array_board[i][j].affirm( [ solving[i][j] ] ) #must pass in list
+
+    # probabilities are in an array of arrays, type probability_array_row
+    # current board is in array called "solving"
+    def scan_row(row):
+        numbers = []
+        for i in solving[row]:
+            if i != 0:
+                numbers.append(i)
+        return numbers
+    
+    def scan_column(column):
+        numbers = []
+        for i in solving[column]:
+            if i != 0:
+                numbers.append(i)
+        return numbers
+
+    
 
 class space_probabilities():
     def __init__ (self):
@@ -33,7 +74,17 @@ class space_probabilities():
     def affirm(self, nums):
         try:
             for possibility in nums:
-                if possibility == 1:
+                if possibility == 0:
+                    self.one = True
+                    self.two = True
+                    self.three = True
+                    self.four = True
+                    self.five = True
+                    self.six = True
+                    self.seven = True
+                    self.eight = True
+                    self.nine = True
+                elif possibility == 1:
                     self.one = True
                 elif possibility == 2:
                     self.two = True
